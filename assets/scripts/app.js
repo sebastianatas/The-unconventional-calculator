31bb1e4913ac286855ca740813be554b5fa4c06f
Sebinatas <sebastianatas@gmail.com>
2023-07-13 09:12:06 +0300

    learned functions and one event listener.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,11 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add() {
+    currentResult = currentResult + userInput.value;
+    
+}
+
+addBtn.addEventListener("click", add)
+
+outputResult(currentResult, calculationDescription);
